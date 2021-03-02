db66fd75cdac9ae427f6085bf663a479db22ab8e
omerxx <omerxx@gmail.com>
2021-03-02 19:22:58 +0200

    removing lib from cent

--- lib/any_hash/src/any_hash/json.cr
+++
@@ -1,9 +0,0 @@
-require "json"
-
-class AnyHash
-  define_new klass: :JSON,
-    key: Symbol | String,
-    value: Nil | Number::Primitive | Bool | Symbol | String | Time | ::JSON::Any
-end
-
-require "./json/*"
--- lib/any_hash/src/any_hash/version.cr
+++
@@ -1,3 +0,0 @@
-class AnyHash
-  VERSION = "0.2.2"
-end
